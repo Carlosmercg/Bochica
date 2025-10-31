0ad8dbb35690d6d2585756c3a3143eb3ff982966
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2025-10-31 06:00:02 -0500

    creacion de proyecto v1

---
+++ front/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'auth/screens/pantalla_inicio.dart';
+
+void main() {
+  runApp(const MiApp());
+}
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mi App Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PantallaInicio(),
+    );
+  }
+}
