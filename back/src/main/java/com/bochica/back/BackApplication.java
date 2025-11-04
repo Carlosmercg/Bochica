0dd52e16c0231c12d5e5ed4de7635c34d0d628fa
Sara Albarracin <114313633+SarAlbN1@users.noreply.github.com>
2025-11-04 17:41:34 -0500

    feat(auth): flujo completo con Firebase (login)

--- back/src/main/java/com/bochica/back/BackApplication.java
+++ back/src/main/java/com/bochica/back/BackApplication.java
@@ -3,11 +3,9 @@ package com.bochica.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bochica")
 public class BackApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BackApplication.class, args);
+  }
 }
